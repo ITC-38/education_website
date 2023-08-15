c8deca39140d565fa448206202ef8928ffe78b52
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-08-15 23:41:26 +0500

    added models Languages/OBjectiveFeatures/Levels  and registred

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Languages, ObjectiveFeatures, Levels
 
-# Register your models here.
+
+@admin.register(Languages)
+class LanguagesAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'slug',)
+    list_display_links = ('name',)
+
+
+@admin.register(ObjectiveFeatures)
+class ObjectiveFeaturesAdmin(ModelAdmin):
+    list_display = ('id', 'name',)
+    list_display_links = ('name',)
+
+
+@admin.register(Levels)
+class LevelsAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'slug',)
+    list_display_links = ('name',)
--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Languages(models.Model):
+    name = models.CharField(
+        max_length=64,
+        unique=True
+    )
+
+    slug = models.SlugField(
+        unique=True
+    )
+
+    native = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Language'
+        verbose_name_plural = 'Languages'
+
+
+class ObjectiveFeatures(models.Model):
+    name = models.CharField(
+        max_length=64,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Objective feature'
+        verbose_name_plural = 'Objective features'
+
+
+class Levels(models.Model):
+    name = models.CharField(
+        max_length=64,
+        unique=True
+    )
+
+    slug = models.SlugField(
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Level'
+        verbose_name_plural = 'Levels'
