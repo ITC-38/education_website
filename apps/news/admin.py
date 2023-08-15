a3ee0a7ae6b308a522d10361a6c426db184edccc
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-08-15 23:46:28 +0500

    added models News and registrated

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import News
 
-# Register your models here.
+
+@admin.register(News)
+class NewsAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'created_date',)
+    list_display_links = ('name',)
+    readonly_fields = ('created_date',)
--- apps/news/models.py
+++ apps/news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    image = models.ImageField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    preview_url = models.URLField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
