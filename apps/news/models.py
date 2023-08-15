94495977b707590f794c9426d72064005e63c37a
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-08-15 23:53:57 +0500

    removed imgae field form News model

--- apps/news/models.py
+++ apps/news/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class News(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    image = models.ImageField(null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     preview_url = models.URLField(null=True, blank=True)
 
