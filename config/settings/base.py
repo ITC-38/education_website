28d96577814cf4c7b1a364edd6554cb82453f078
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-08-22 13:51:02 +0500

    hotfix/templates-static-restucture

--- config/settings/base.py
+++ config/settings/base.py
@@ -36,7 +36,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,7 +78,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'apps/accounts/static'
+    BASE_DIR / 'staticfiles',
 ]
 
 MEDIA_URL = 'media/'
